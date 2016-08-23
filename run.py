ab5521108a07c3108a5972ae1d6c2d658bdc6de6
AB49K <andrewberry4900@hotmail.com>
2016-08-23 13:00:04 +1000

    Delete run.py

--- run.py
+++
@@ -1,5 +0,0 @@
-import subprocess 
-
-while True:
-    command = input("PELL~> ") # We can add a configurable prompt later
-    subprocess.call(['/bin/' + command]) # Just a test, this lets you run things in /bin such as ls
