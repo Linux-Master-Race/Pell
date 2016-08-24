cbab39af7cd16e71a74e7d9c4d052e1a51fb4f71
Andrew Hall <andrew.hall649@gmail.com>
2016-08-24 11:43:30 +1000

    Config loader skeleton file [pt2]

--- src/confloader.py
+++ src/confloader.py
@@ -7,19 +7,31 @@ class PellConfig:
             self,
             loadconf=True,
             loadrc=False,
-            rcpath='~/.pellrc)',
+            rcpath='~/.pellrc',
             confpath='~/.pell.conf'):
 
-        self.__config = {}
+        self.__config={}
+        self.__defconfpath="/etc/pell/pell.conf"
+        self.__defrcpath="/etc/pell/pellrc"
 
-    def get_option(self, optionname, defvalue):
+    def get_option(self, optname, defvalue):
         pass
 
-    def set_option(self, optionname, value):
+    def set_option(self, optname, value):
         pass
 
-    def parse_rc(self, executecode=False):
+    def parse_rc(self, execrc=False):
         pass
 
     def parse_conf(self):
         pass
+
+    def set_rc_path(self, rcpath):
+        pass
+
+    def set_conf_path(self, confpath):
+        pass
+
+    def as_dict(self):
+        return self.__config
+
