099555f277a9be3e63352dd6f91d6f92ecd75a39
Andrew Hall <andrew.hall649@gmail.com>
2016-08-24 12:13:08 +1000

    Complete config skeleton file.

--- src/confloader.py
+++ src/confloader.py
@@ -5,33 +5,49 @@ class PellConfig:
 
     def __init__(
             self,
-            loadconf=True,
-            loadrc=False,
-            rcpath='~/.pellrc',
-            confpath='~/.pell.conf'):
+            loadconf=True,  # Load the config file
+            loadrc=False,  # Load the rc file
+            confpath='~/.pell.conf',  # Path to the conf file
+            rcpath='~/.pellrc'):  # Path to the rc file
 
-        self.__config = {}
-        self.__defconfpath = "/etc/pell/pell.conf"
-        self.__defrcpath = "/etc/pell/pellrc"
+        self.__config = {}  # Contains the keys and values for the options.
+        self.__defconfpath = "/etc/pell/pell.conf"  # Default conf path
+        self.__defrcpath = "/etc/pell/pellrc"  # Default rc path
 
+    # Returns the value of an option or a default if the option doesn't exist.
     def get_option(self, optname, defvalue):
         pass
 
+    # Sets an option to the sepecified value
     def set_option(self, optname, value):
         pass
 
+    # Parses the rc file set in the initialiser.
+    # If execrc is set to True, execute any functions inside the rc file.
     def parse_rc(self, execrc=False):
         pass
 
+    # Parses the conf file set in the initialiser with the json module.
     def parse_conf(self):
         pass
 
+    # Gets the path to the rc file.
+    def get_rc_path(self):
+        pass
+
+    # Gets the path to the conf file.
+    def get_conf_path(self):
+        pass
+
+    # Sets the path to the rc file.
     def set_rc_path(self, rcpath):
         pass
 
+    # Sets the path to the conf file.
     def set_conf_path(self, confpath):
         pass
 
+    # Return the config as a dictionary.
     def as_dict(self):
         return self.__config
 
